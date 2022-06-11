8cfe65a0f5636099b3dcaff6ab3ea18dda0ad800
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2022-06-11 13:08:51 +0600

    added color mode

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-let magicNumber,grid,gridSize,colorGL;
+var magicNumber,gridL = 10,gridSize,colorGL = "#ff0000",randomColor="#256fe2";
 
-function paint(id){
-    white = document.getElementById(id);
-    white.style.backgroundColor = "blue";
+window.onload = function(){run();}
+setInterval(random, 1000);
+
+function random(){
+    randomColor = Math.floor(Math.random()*16777215).toString(16);
+    console.log(randomColor);
 }
+
+document.getElementById("gs").onclick = function(){promptGrid();}
+document.getElementById("clr").onclick = function(){clearButton();}
+document.getElementById("color").onclick = function(){test();}
+document.getElementById("rainbow").onclick = function(){goRainbow();}
+
+var colorB =  document.getElementById("color");
+colorB.addEventListener("change", updateColor);
+
+function run(){
+    createGrids(10);
+    test();
+}
+
+function updateColor(e){
+    colorGL = e.target.value;
+}
+
+function test(){
+    for(i=0;i<gridL*gridL;i++){
+        document.getElementById(i+"n").onmouseenter = function(event){
+            event.target.style.backgroundColor = colorGL;
+        };
+    }
+}
+
+
+function goRainbow(){
+    for(i=0;i<gridL*gridL;i++){
+        document.getElementById(i+"n").onmouseenter = function(event){
+            event.target.style.backgroundColor = randomColor;
+        };
+    }
+}
+
+
+
 function clear(){
     
     const myNode = document.getElementById("canvas");
@@ -11,6 +51,11 @@ function clear(){
         myNode.removeChild(myNode.lastChild);
     }
     
+}
+
+function clearButton(){
+    clear();
+    createGrids(gridL);
 }
 
 function promptGrid(){
@@ -21,10 +66,10 @@ function promptGrid(){
 
 function createGrids(gridSize){
     const full = document.getElementById("canvas");
+    gridL = gridSize;
     for(i=0;i<gridSize*gridSize;i++)
     {
         magicNumber = 512/gridSize;
-        console.log(magicNumber);
         magicNumber = magicNumber + "px";
         n = document.createElement('div');
         n.style.minWidth = magicNumber;
@@ -33,10 +78,12 @@ function createGrids(gridSize){
         n.style.maxHeight = magicNumber;
         n.classList.add("paint-on");
         id = i+"";
-        n.setAttribute("id","id");
+        n.setAttribute("id",id+"n");
         full.appendChild(n);
     }
+    test();
 }
 
-document.getElementById("gs").onclick = function(){promptGrid();}
-document.getElementsByClassName("paint-on").onmouseover = function(){paint(this.id);}
+
+
+
